a26ac5386d99ab97f4ff904c964a2068957bca66
Alejandro Obando Gil <alejo.obando.g@gmail.com>
2024-07-22 11:19:36 -0500

    fix

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -22,12 +22,15 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             })
     );
 
-    const csrf = () => axios.get('/sanctum/csrf-cookie');
+    const csrf = () => {
+        axios.get('/sanctum/csrf-cookie');
+        console.log(document.cookie);
+    }
 
     function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
+        // const value = `; ${document.cookie}`;
+        // const parts = value.split(`; ${name}=`);
+        // if (parts.length === 2) return parts.pop().split(';').shift();
     }
 
     const register = async ({ setErrors, ...props }) => {

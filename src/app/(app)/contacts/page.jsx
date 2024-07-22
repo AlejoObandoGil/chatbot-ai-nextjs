831b724bdb1c3fd97e32c0d17f96c9452966c772
Alejandro Obando Gil <alejo.obando.g@gmail.com>
2024-07-21 21:56:08 -0500

    feat (contact-information): index clients with contact information

--- src/app/(app)/contacts/page.jsx
+++ src/app/(app)/contacts/page.jsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { MagnifyingGlassIcon, ChevronUpDownIcon } from '@heroicons/react/24/outline';
-import { PencilIcon } from '@heroicons/react/24/solid';
+import axios from '@/lib/axios';
 import {
     Card,
     CardHeader,
@@ -9,201 +10,179 @@ import {
     Typography,
     Button,
     CardBody,
-    Chip,
     CardFooter,
     Tabs,
     TabsHeader,
     Tab,
-    Avatar,
-    IconButton,
-    Tooltip
+    Tooltip,
+    Select,
+    Option
 } from '@material-tailwind/react';
 
-const TABS = [
-    {
-        label: 'All',
-        value: 'all'
-    },
-    {
-        label: 'Monitored',
-        value: 'monitored'
-    },
-    {
-        label: 'Unmonitored',
-        value: 'unmonitored'
-    }
-];
-
-const TABLE_HEAD = ['Member', 'Function', 'Status', 'Employed', ''];
-
-const TABLE_ROWS = [
-    {
-        img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg',
-        name: 'John Michael',
-        email: 'john@creative-tim.com',
-        job: 'Manager',
-        org: 'Organization',
-        online: true,
-        date: '23/04/18'
-    },
-    {
-        img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg',
-        name: 'Alexa Liras',
-        email: 'alexa@creative-tim.com',
-        job: 'Programator',
-        org: 'Developer',
-        online: false,
-        date: '23/04/18'
-    },
-    {
-        img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg',
-        name: 'Laurent Perrier',
-        email: 'laurent@creative-tim.com',
-        job: 'Executive',
-        org: 'Projects',
-        online: false,
-        date: '19/09/17'
-    },
-    {
-        img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg',
-        name: 'Michael Levi',
-        email: 'michael@creative-tim.com',
-        job: 'Programator',
-        org: 'Developer',
-        online: true,
-        date: '24/12/08'
-    },
-    {
-        img: 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg',
-        name: 'Richard Gran',
-        email: 'richard@creative-tim.com',
-        job: 'Manager',
-        org: 'Executive',
-        online: false,
-        date: '04/10/21'
-    }
-];
-
 export default function SortableTable() {
-    return (
-        <Card className="h-full w-full">
-            <CardHeader floated={false} shadow={false} className="rounded-none">
-                <div className="mb-8 flex items-center justify-between gap-8">
-                    <div>
-                        <Typography variant="h5" color="blue-gray">
-                            Members list
-                        </Typography>
-                        <Typography color="gray" className="mt-1 font-normal">
-                            See information about all members
-                        </Typography>
-                    </div>
-                    <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-                        <Button variant="outlined" size="sm">
-                            view all
-                        </Button>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-                    <Tabs value="all" className="w-full md:w-max">
-                        <TabsHeader>
-                            {TABS.map(({ label, value }) => (
-                                <Tab key={value} value={value}>
-                                    &nbsp;&nbsp;{label}&nbsp;&nbsp;
-                                </Tab>
-                            ))}
-                        </TabsHeader>
-                    </Tabs>
-                    <div className="w-full md:w-72">
-                        <Input label="Search" icon={<MagnifyingGlassIcon className="h-5 w-5" />} />
-                    </div>
-                </div>
-            </CardHeader>
-            <CardBody className="overflow-scroll px-0">
-                <table className="mt-4 w-full min-w-max table-auto text-left">
-                    <thead>
-                        <tr>
-                            {TABLE_HEAD.map((head, index) => (
-                                <th
-                                    key={head}
-                                    className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        color="blue-gray"
-                                        className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
-                                    >
-                                        {head} {index !== TABLE_HEAD.length - 1 && <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />}
-                                    </Typography>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {TABLE_ROWS.map(({ img, name, email, job, org, online, date }, index) => {
-                            const isLast = index === TABLE_ROWS.length - 1;
-                            const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
+    const [columns, setColumns] = useState([]);
+    const [rows, setRows] = useState([]);
+    const [chatbots, setChatbots] = useState([]);
+    const [selectedChatbot, setSelectedChatbot] = useState('');
 
-                            return (
-                                <tr key={name}>
-                                    <td className={classes}>
-                                        <div className="flex items-center gap-3">
-                                            <Avatar src={img} alt={name} size="sm" />
-                                            <div className="flex flex-col">
-                                                <Typography variant="small" color="blue-gray" className="font-normal">
-                                                    {name}
-                                                </Typography>
-                                                <Typography variant="small" color="blue-gray" className="font-normal opacity-70">
-                                                    {email}
-                                                </Typography>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td className={classes}>
-                                        <div className="flex flex-col">
-                                            <Typography variant="small" color="blue-gray" className="font-normal">
-                                                {job}
-                                            </Typography>
-                                            <Typography variant="small" color="blue-gray" className="font-normal opacity-70">
-                                                {org}
-                                            </Typography>
-                                        </div>
-                                    </td>
-                                    <td className={classes}>
-                                        <div className="w-max">
-                                            <Chip variant="ghost" size="sm" value={online ? 'online' : 'offline'} color={online ? 'green' : 'blue-gray'} />
-                                        </div>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography variant="small" color="blue-gray" className="font-normal">
-                                            {date}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Tooltip content="Edit User">
-                                            <IconButton variant="text">
-                                                <PencilIcon className="h-4 w-4" />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </CardBody>
-            <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-                <Typography variant="small" color="blue-gray" className="font-normal">
-                    Page 1 of 10
+    useEffect(() => {
+        const fetchChatbots = async () => {
+            try {
+                const { data } = await axios.get('/api/v1/chatbot');
+                setChatbots(data.chatbots);
+                console.log('Chatbots:', data.chatbots);
+            } catch (error) {
+                console.error('Error fetching chatbots:', error);
+            }
+        };
+
+        fetchChatbots();
+    }, []);
+
+    useEffect(() => {
+        if (selectedChatbot) {
+            const fetchContactInformations = async () => {
+                try {
+                    const { data } = await axios.get(`/api/v1/chatbot/${selectedChatbot}/contact-information`);
+
+                    const columns = data.intents.map(intent => intent.name);
+                    columns.push('Acciones');
+                    const rows = data.intents.reduce((rowsIntent, intent) => {
+                        intent.contact_informations.forEach((contact, index) => {
+                            if (!rowsIntent[index]) {
+                                rowsIntent[index] = { status: contact.status || 'sin contactar' };
+                            }
+
+                            rowsIntent[index][intent.name] = contact.value;
+                        });
+                        return rowsIntent;
+                    }, []);
+
+                    setColumns(columns);
+                    setRows(rows);
+
+                    console.log('rows:', rows);
+                } catch (error) {
+                    console.error('Error fetching contact information:', error);
+                }
+            };
+
+            fetchContactInformations();
+        }
+    }, [selectedChatbot]);
+
+    const handleStatusChange = (index) => {
+        setRows(prevRows => {
+            const updatedRows = [...prevRows];
+            const currentStatus = updatedRows[index].status;
+            const statusOrder = ['sin contactar', 'contacto exitoso', 'contacto no exitoso'];
+            const newStatus = statusOrder[(statusOrder.indexOf(currentStatus) + 1) % statusOrder.length];
+            updatedRows[index].status = newStatus;
+            return updatedRows;
+        });
+    };
+
+    const getStatusColor = (status) => {
+        if (status === 'contacto exitoso') return 'bg-green-500';
+        if (status === 'contacto no exitoso') return 'bg-red-500';
+        return 'bg-gray-500';
+    };
+
+    return (
+        <Card className="h-full w-full p-6">
+            <div className="mb-6 flex items-center justify-between">
+                <Typography variant="h5" color="blue-gray">
+                    Contact Information
                 </Typography>
-                <div className="flex gap-2">
-                    <Button variant="outlined" size="sm">
-                        Previous
-                    </Button>
-                    <Button variant="outlined" size="sm">
-                        Next
-                    </Button>
-                </div>
-            </CardFooter>
+            </div>
+            <div className="w-1/3 mb-6">
+                <Select
+                    label="Selecciona un Chatbot"
+                    value={selectedChatbot}
+                    onChange={(value) => setSelectedChatbot(value)}
+                >
+                    {chatbots.map((chatbot) => (
+                        <Option key={chatbot.id} value={chatbot.id}>
+                            {chatbot.name}
+                        </Option>
+                    ))}
+                </Select>
+            </div>
+            {selectedChatbot ? (
+                <>
+                    <CardHeader floated={false} shadow={false} className="rounded-none mb-4">
+                        <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                            <div className="w-full md:w-72">
+                                <Input label="Search" icon={<MagnifyingGlassIcon className="h-5 w-5" />} />
+                            </div>
+                        </div>
+                    </CardHeader>
+                    <CardBody className="overflow-scroll px-0 mb-4">
+                        <table className="mt-4 w-full min-w-max table-auto text-left">
+                            <thead>
+                                <tr>
+                                    {columns.map((column) => (
+                                        <th
+                                            key={column}
+                                            className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                                        >
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                                            >
+                                                {column} <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                                            </Typography>
+                                        </th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {rows.map((row, index) => {
+                                    const isLast = index === rows.length - 1;
+                                    const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
+
+                                    return (
+                                        <tr key={index}>
+                                            {columns.map((column, colIndex) => (
+                                                <td key={colIndex} className={classes}>
+                                                    <Typography variant="small" color="blue-gray" className="font-normal">
+                                                        {column !== 'Acciones' ? row[column] : (
+                                                            <Button
+                                                                className={`text-white ${getStatusColor(row.status)}`}
+                                                                onClick={() => handleStatusChange(index)}
+                                                            >
+                                                                {row.status}
+                                                            </Button>
+                                                        )}
+                                                    </Typography>
+                                                </td>
+                                            ))}
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </CardBody>
+                    <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            Page 1 of 10
+                        </Typography>
+                        <div className="flex gap-2">
+                            <Button variant="outlined" size="sm">
+                                Previous
+                            </Button>
+                            <Button variant="outlined" size="sm">
+                                Next
+                            </Button>
+                        </div>
+                    </CardFooter>
+                </>
+            ) : (
+                <Typography variant="h6" color="blue-gray" className="text-center mt-4">
+                    Selecciona un chatbot para listar la información de contacto de tus clientes
+                </Typography>
+            )}
         </Card>
     );
 }

4b70604ba9931fe464bb64ce137e58a89cb94d2e
Alejandro Obando Gil <alejo.obando.g@gmail.com>
2024-08-11 22:59:51 -0500

    fix: charts ssr

--- src/app/(app)/dashboard/page.jsx
+++ src/app/(app)/dashboard/page.jsx
@@ -8,34 +8,8 @@ import {
 } from '@material-tailwind/react';
 
 const Dashboard = () => {
-
-    // const [text, setText] = useState('');
-
-//   const startRecognition = () => {
-//     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-//     const recognition = new SpeechRecognition();
-
-//     recognition.onstart = () => {
-//       console.log('Voice recognition started. Speak into the microphone.');
-//     };
-
-//     recognition.onresult = (event) => {
-//       const transcript = event.results[0][0].transcript;
-//       setText(transcript);
-//       console.log('Transcript:', transcript);
-//     };
-
-//     recognition.onerror = (event) => {
-//       console.error('Error occurred in recognition: ', event.error);
-//     };
-
-//     recognition.start();
-//   };
-
     return (
         <>
-            {/* <button onClick={startRecognition}>Start Voice Recognition</button>
-            <p>Text: {text}</p> */}
             <Typography variant="h5" color="indigo" className="text-center mb-3">
                 Inicio - Uso de Chatbots
             </Typography>
--- src/components/charts/LineChart.jsx
+++ src/components/charts/LineChart.jsx
@@ -1,15 +1,15 @@
+'use client';
+
 import {
     Card,
     CardBody,
     CardHeader,
     Typography,
 } from "@material-tailwind/react";
-import Chart from "react-apexcharts";
+import dynamic from "next/dynamic";
 import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
 
-// If you're using Next.js please use the dynamic import for react-apexcharts and remove the import from the top for the react-apexcharts
-// import dynamic from "next/dynamic";
-// const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const chartConfig = {
     type: "line",
